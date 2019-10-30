2c37c867adf4129f79c5bdc20284a873fab12f19
ariqfikri <lolicodesu@gmail.com>
2019-10-30 21:30:37 +0700

    update frontend

---
+++ frontend/pages/signup.vue
@@ -0,0 +1,46 @@
+<template>
+  <v-app id="inspire">
+    <form-login
+      :title="title"
+      :form="form"
+      :width="width"
+      :namebutton="namebutton"
+      :loc="loc"
+    ></form-login>
+  </v-app>
+</template>
+<script>
+export default {
+  layout: 'empty',
+  data() {
+    return {
+      loc: '/login',
+      title: 'Create your account',
+      width: '9.4cm',
+      namebutton: 'Agree and Sign up',
+      form: [
+        {
+          label: 'Email',
+          name: 'email',
+          model: 'email'
+        },
+        {
+          label: 'Password',
+          name: 'password',
+          model: 'password'
+        },
+        {
+          label: 'Firstname',
+          name: 'firstname',
+          model: 'firstname'
+        },
+        {
+          label: 'Lastname',
+          name: 'lasname',
+          model: 'lastname'
+        }
+      ]
+    }
+  }
+}
+</script>
